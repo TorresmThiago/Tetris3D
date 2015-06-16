1d6bb0e74bf2c42a614a4b97bb3674b5086abd33
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-16 08:54:44 -0300

    Projeto
    Pulamos alguns commits... Acontece.

---
+++ Tetris3D/Assets/_Scripts/Rotate_Obj.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_Obj : MonoBehaviour {
+
+	private float rotLeft;
+	private float rotRight;
+	private bool isRotating;
+	private string direction;
+
+	void Swing(float rot)
+	{   
+		Quaternion newRotation = Quaternion.AngleAxis (rot, Vector3.up);
+		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, 0.1f);   
+	}
+
+	void FixAngle(string direction){
+
+		if (direction == "left") {
+			switch ((int)transform.localEulerAngles.y) {
+			case 91:
+				transform.rotation = Quaternion.Euler(0, 90, 0);
+				break;
+			case 181:
+				transform.rotation = Quaternion.Euler(0, 180, 0);
+				break;
+			case 271:
+				transform.rotation = Quaternion.Euler(0, 270, 0);
+				break;
+			case 1:
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+				break;
+			default:
+				break;
+			}
+		} else if (direction == "right") {
+			switch ((int)transform.localEulerAngles.y) {
+			case 89:
+				transform.rotation = Quaternion.Euler(0, 90, 0);
+				break;
+			case 179:
+				transform.rotation = Quaternion.Euler(0, 180, 0);
+				break;
+			case 269:
+				transform.rotation = Quaternion.Euler(0, 270, 0);
+				break;
+			case 359:
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void Rotate(string dir){
+		if (dir == "left") {
+			Swing (rotLeft);
+		} else if(dir == "right") {
+			Swing(rotRight);
+		}
+	}
+
+	void checkAngle(){
+		switch ((int)transform.localEulerAngles.y) {
+		case 0:
+			rotLeft = -90;
+			rotRight = 90;
+			isRotating = false;	
+			break;
+		case 90:
+			rotLeft = 0;
+			rotRight = -180;
+			isRotating = false;
+			break;			
+		case 180:
+			rotLeft = 90;
+			rotRight = 270;
+			isRotating = false;	
+			break;
+		case 270:
+			rotLeft = 180;
+			rotRight = -360;
+			isRotating = false;	
+			break;
+		default:
+			break;
+		}
+	}
+	
+	void Update (){
+
+		if (!isRotating) {
+			if (Input.GetKey (KeyCode.C)) {
+				direction = "left";
+				isRotating = true;
+			} else if (Input.GetKey (KeyCode.V)) {
+				direction = "right";
+				isRotating = true;
+			}
+		} else if(isRotating) {
+			Rotate(direction);
+			FixAngle(direction);
+			checkAngle();
+		}
+	}
+
+}
---
+++ Tetris3D/Assets/_Scripts/Tile_Mov.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile_Mov : MonoBehaviour {
+
+	public GameObject parent;
+	private bool isMoving = true;
+	private bool isWorking = true;
+
+	IEnumerator MovVertical (){
+		if (isMoving) {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y - 1, gameObject.transform.position.z);
+			yield return new WaitForSeconds (1.5f);
+			StartCoroutine (MovVertical ());
+		}
+	}
+
+	IEnumerator BlockMov(){
+		yield return new WaitForSeconds (0.7f);
+		this.transform.parent = parent.transform;
+		isWorking = false;
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Board"){
+			isMoving = false;
+			StartCoroutine (BlockMov ());
+		}
+	}
+
+	void rotate(){
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			gameObject.transform.Rotate (0, 0, -90);
+		} else if (Input.GetKeyDown (KeyCode.X)) {
+			gameObject.transform.Rotate (0, 0, 90);
+		}
+	}
+
+	void MovHorizontal(){
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && gameObject.transform.position.x != -4.5f) {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+		} else if(Input.GetKeyDown (KeyCode.RightArrow) && gameObject.transform.position.x != 4.5f){
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+	}
+
+	void Start () {
+		StartCoroutine (MovVertical ());
+	}
+
+	void Update () {
+		if (isWorking) {
+			rotate ();
+			MovHorizontal ();
+		}
+	}
+}
